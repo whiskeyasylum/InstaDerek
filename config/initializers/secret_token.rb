5bd42a43072ed9f2a720764a6715934ed63f65e8
Mike Benner <refriedchicken@gmail.com>
2011-01-25 19:28:25 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instaderek::Application.config.secret_token = '72428e6eae36d855e9a8c69c6c3b2ef5332427bc373b73da63b57260214482cf2ff57e409687761c0f0e68049b721f086109bfdd9da3f13909b25189611993e2'
