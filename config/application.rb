524bf6baefdfc6f65a56e1e38b2de7d44aaa1142
Roy van de Water <roy@Doppler.(none)>
2011-02-06 00:38:31 -0800

    Added fancybox to the image gallery

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module Instaderek
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
+    # config.action_view.javascript_expansions[:defaults] = %w(rails fancybox/jquery.fancybox-1.3.4)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
